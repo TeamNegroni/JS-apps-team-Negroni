8e2c9bf903d75c2856a60886ea6fd477c38b0639
Georgegig <george_gig@abv.bg>
2015-08-26 15:12:29 +0300

    Added checkbox controller to control the date

--- Notebook/Notebook/Notebook/Scripts/uiController.js
+++ Notebook/Notebook/Notebook/Scripts/uiController.js
@@ -1,6 +1,18 @@
-﻿var currentDate = new Date();
+﻿//Controlling the date at the UI Pannel
+var currentDate = new Date();
 $("#current-date").attr('placeholder', currentDate.getDate() + "/"
     + currentDate.getMonth() + "/" + currentDate.getFullYear());
+
 $(function () {
     $("#date-picker").datepicker();
+});
+
+$('#date-controller-checkbox').change(function () {
+    if ($(this).is(':checked')) {
+        $('#current-date').hide();
+        $('#date-picker').show();
+    } else {
+        $('#current-date').show();
+        $('#date-picker').hide();
+    }
 });
