ba1f3f115ab347e416ba2434c669b1a07f734fa3
Georgegig <george_gig@abv.bg>
2015-08-26 17:18:52 +0300

    Unsuccessful creation of drag and drop objects

--- Notebook/Notebook/Notebook/Scripts/uiController.js
+++ Notebook/Notebook/Notebook/Scripts/uiController.js
@@ -1,18 +1,31 @@
-﻿//Controlling the date at the UI Pannel
+﻿var $datePicker = $("#date-picker");
+var $currentDateController = $("#current-date");
 var currentDate = new Date();
-$("#current-date").attr('placeholder', currentDate.getDate() + "/"
+var $dateControllerCheckbox = $('#date-controller-checkbox');
+var $draggableComponents = $('.draggable-component');
+
+//Controlling the date at the UI Pannel
+$currentDateController.attr('placeholder', currentDate.getDate() + "/"
     + currentDate.getMonth() + "/" + currentDate.getFullYear());
 
+//Initiate datepicker
 $(function () {
-    $("#date-picker").datepicker();
+    $datePicker.datepicker();
 });
 
-$('#date-controller-checkbox').change(function () {
+//Change UI date of the note options
+$dateControllerCheckbox.change(function () {
     if ($(this).is(':checked')) {
-        $('#current-date').hide();
-        $('#date-picker').show();
+        $currentDateController.hide();
+        $datePicker.show();
     } else {
-        $('#current-date').show();
-        $('#date-picker').hide();
+        $currentDateController.show();
+        $datePicker.hide();
     }
 });
+
+//Control draggables
+$(function () {
+    $draggableComponents.draggable({ containment: "parent" });
+});
+
