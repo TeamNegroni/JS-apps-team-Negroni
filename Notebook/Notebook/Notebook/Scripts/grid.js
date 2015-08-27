a2b5bd14aeda31eb198b0e16b14e02ce835c6c76
Georgegig <george_gig@abv.bg>
2015-08-28 00:58:52 +0300

    Added textarea input

--- Notebook/Notebook/Notebook/Scripts/grid.js
+++ Notebook/Notebook/Notebook/Scripts/grid.js
@@ -2,7 +2,13 @@
     var $grid = $('#grid'),
         $adder = $('#gridAdder');
     var index = 1;
-    var $inputType =$('.input-type');
+    var $inputType = $('.input-type');
+    var $inputTypeImage = $('.input-type-image');
+    var $inputTypeTextarea = $('.input-type-textarea');
+    var $inputTypeIssueNote = $('.input-type-issue-note');
+    var $inputTypeMeetingNote = $('.input-type-meeting-note');
+    var $inputTypeBankNote = $('.input-type-bank-note');
+    var $inputTypeShoppingNote = $('.input-type-shopping-note');
 
     $grid.sortable({
         containment: 'parent',
@@ -35,25 +41,47 @@
    //     $newPiece.insertBefore('#gridAdder');
    //     $newPiece.show(500);
    // });
-    $inputType.each(function () {
-        $(this).on('click', function () {
-            var $newPiece = $('<li/>');
-            $newPiece.addClass('gridPiece');
+    //$inputType.each(function () {
+    //    $(this).on('click', function () {
+    //        var $newPiece = $('<li/>');
+    //        $newPiece.addClass('gridPiece');
+    //
+    //        $newPiece.text(index);
+    //        index += 1;
+    //
+    //        $newPiece.resizable({
+    //            grid: [362, 362], // value to be edited
+    //            autoHide: true,
+    //            animate: true,
+    //            helper: "resizable-helperPiece",
+    //            animateEasing: "easeInOutQuint"
+    //        });
+    //
+    //        $newPiece.hide();
+    //        $newPiece.insertBefore('#gridAdder');
+    //        $newPiece.show(500);
+    //    });
+    //})
 
-            $newPiece.text(index);
-            index += 1;
-
-            $newPiece.resizable({
-                grid: [362, 362], // value to be edited
-                autoHide: true,
-                animate: true,
-                helper: "resizable-helperPiece",
-                animateEasing: "easeInOutQuint"
-            });
-
-            $newPiece.hide();
-            $newPiece.insertBefore('#gridAdder');
-            $newPiece.show(500);
+    $inputTypeTextarea.on('click', function () {
+        var $newPiece = $('<li/>');
+        var $textArea = $('<textarea>').addClass('form-control').attr('row', 3).css('max-width',300);
+        $newPiece.addClass('gridPiece');
+        
+        $newPiece.text(index);
+        index += 1;
+        
+        $newPiece.resizable({
+            grid: [362, 362], // value to be edited
+            autoHide: true,
+            animate: true,
+            helper: "resizable-helperPiece",
+            animateEasing: "easeInOutQuint"
         });
-    })
+        
+        $newPiece.append($textArea);
+        $newPiece.hide();
+        $newPiece.insertBefore('#gridAdder');
+        $newPiece.show(500);
+    });
 }());
