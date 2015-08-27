c0856819cbd5b2b4d8ec252de62ee39d32003cd7
Georgegig <george_gig@abv.bg>
2015-08-28 01:12:56 +0300

    Added input type FILE for the IMAGE input

--- Notebook/Notebook/Notebook/Scripts/grid.js
+++ Notebook/Notebook/Notebook/Scripts/grid.js
@@ -65,7 +65,7 @@
 
     $inputTypeTextarea.on('click', function () {
         var $newPiece = $('<li/>');
-        var $textArea = $('<textarea>').addClass('form-control').attr('row', 3).css('max-width',300);
+        var $textArea = $('<textarea/>').addClass('form-control').attr('row', 3).css('max-width',300);
         $newPiece.addClass('gridPiece');
         
         $newPiece.text(index);
@@ -84,4 +84,49 @@
         $newPiece.insertBefore('#gridAdder');
         $newPiece.show(500);
     });
+
+    $inputTypeImage.on('click', function () {
+        var $newPiece = $('<li/>');
+        var $input = $('<input/>').attr('type', 'file');
+        var $span = $('<span/>').addClass('file-input').addClass('btn').addClass('btn-primary').addClass('btn-file').html('Browse').append($input);
+        $newPiece.addClass('gridPiece');
+
+        $newPiece.text(index);
+        index += 1;
+
+        $newPiece.resizable({
+            grid: [362, 362], // value to be edited
+            autoHide: true,
+            animate: true,
+            helper: "resizable-helperPiece",
+            animateEasing: "easeInOutQuint"
+        });
+
+        $newPiece.append($span);
+        $newPiece.hide();
+        $newPiece.insertBefore('#gridAdder');
+        $newPiece.show(500);
+    });   
 }());
+
+$(document).on('change', '.btn-file :file', function () {
+    var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [numFiles, label]);
+});
+
+$(document).ready(function () {
+    $('.btn-file :file').on('fileselect', function (event, numFiles, label) {
+
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+        if (input.length) {
+            input.val(log);
+        } else {
+            if (log) alert(log);
+        }
+
+    });
+});
