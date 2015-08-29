8acb74e8b4b2b672b0544c62633dfa91a2c3f902
kossov <ogi_2112@yahoo.com>
2015-08-29 18:39:15 +0300

    This closes #6, previous, next month now properly displayed

--- calendar.js
+++ calendar.js
@@ -35,6 +35,7 @@
         var date = new Date(todaysDate);    // you are welcome :)
         console.log(date.getDayName());
         console.log(date.getMonthName());
+        console.log(date.getDate());
 
         var $controls = $('<div/>'),
             $button = $('<button/>'),
@@ -45,7 +46,7 @@
         var $currentDateLink = $('<div/>');
 
         // setting up controls part
-            // TODO: moving between months when pressing arrows
+        // TODO: moving between months when pressing arrows
         $controls.addClass('controls');
         $button.addClass('btn');
         $currentMonth.addClass('current-month');
@@ -54,14 +55,14 @@
         $controls.append($button.clone().text('>'));
 
         // setting up the table with days
-            // TODO: when a day is pressed must ask the server for data on that day
-        $daysTable.addClass('.daysTable');
+        // TODO: when a day is pressed must ask the server for data on that day
+        $daysTable.addClass('daysTable');
         headTemplate = buildHeadTemplate();
-        daysTemplate = refreshDays(); // might need to be outside this scope for update purposes
+        daysTemplate = refreshDays('current-day', 'current-month', 'another-month'); // might need to be outside this scope for update purposes
         $daysTable.html(headTemplate + daysTemplate);
 
         // setting up the lowest(current day) part
-            // TODO: add onclick to focus the today's date in the table
+        // TODO: add onclick to focus the today's date in the table
         $currentDateLink
             .addClass('current-date-link')
             .addClass('current-date');
@@ -93,12 +94,47 @@
             return days;
         }
 
-        function refreshDays() {
-            var days = getDaysInMonth(date.getMonth(), date.getFullYear());
-            var daysTableTemplate = '',
+        function refreshDays(currentDayCssClass, currentMonthCssClass, anotherMonthCssClass) {
+            var currentMonthDays = getDaysInMonth(date.getMonth(), date.getFullYear()),
+                daysTableTemplate = '',
                 inRow = false;
-            for (var i = 0; i < days.length; i++) {
-                var day = days[i].getDate();
+            var currentMonth = date.getMonth(),
+                previousMonthDays,
+                nextMonthDays;
+
+            // might refactor to get only the count of the days in month
+            if (currentMonth === 0) {
+                previousMonthDays = getDaysInMonth(11, date.getFullYear() - 1);
+                nextMonthDays = getDaysInMonth(1, date.getFullYear());
+            } else if (currentMonth === 11) {
+                previousMonthDays = getDaysInMonth(10, date.getFullYear());
+                nextMonthDays = getDaysInMonth(0, date.getFullYear() + 1);
+            } else {
+                previousMonthDays = getDaysInMonth(currentMonth - 1, date.getFullYear() - 1);
+                nextMonthDays = getDaysInMonth(currentMonth + 1, date.getFullYear());
+            }
+
+            var currentMonthDaysIndex = 0,
+                nextMonthDaysIndex = 0;
+            for (var i = 0; i < 42; i++) {
+                var day,
+                    inCurrentMonth,
+                    cssClass;
+
+                if ((currentMonthDaysIndex !== 0 && currentMonthDaysIndex < currentMonthDays.length) ||
+                    currentMonthDays[0].getDay() === i) {
+                    inCurrentMonth = true;
+                    day = currentMonthDays[currentMonthDaysIndex].getDate();
+                    currentMonthDaysIndex += 1;
+                } else if (currentMonthDaysIndex === 0) {
+                    inCurrentMonth = false;
+                    day = previousMonthDays[previousMonthDays.length - (currentMonthDays[0].getDay() + 1) + (i + 1)].getDate();
+                } else {
+                    inCurrentMonth = false;
+                    day = nextMonthDays[nextMonthDaysIndex].getDate();
+                    nextMonthDaysIndex += 1;
+                }
+
                 if (i % 7 === 0) {
                     if (!inRow) {
                         daysTableTemplate += '<tr>';
@@ -109,7 +145,17 @@
                     }
                 }
 
-                daysTableTemplate += '<td>' + day + '</td>';
+                if (inCurrentMonth) {
+                    if (date.getDate() === day) {
+                        cssClass = currentMonthCssClass + ' ' + currentDayCssClass;
+                    } else {
+                        cssClass = currentMonthCssClass;
+                    }
+                } else {
+                    cssClass = anotherMonthCssClass;
+                }
+
+                daysTableTemplate += '<td class="' + cssClass + '">' + day + '</td>';
             }
 
             return daysTableTemplate;
