9cd020f3fefb8b71dae11781673d7151e00db3b2
Georgegig <george_gig@abv.bg>
2015-08-30 23:37:54 +0300

    Deleted unnecessary lines

--- Notebook/Notebook/Notebook/Scripts/grid.js
+++ Notebook/Notebook/Notebook/Scripts/grid.js
@@ -12,12 +12,9 @@
 
     function generateNoteDiv(id) {
         var noteDiv = $('<div/>').attr('class', 'note' + id);
-        var noteTitle = $('<input/>');
-        noteTitle.attr('class', 'note-title');
-        noteTitle.attr('placeholder', 'Title');
+        var noteTitle = $('<input/>').attr('class', 'note-title').attr('placeholder', 'Title');
         var noteContent = $('<textarea/>').attr('rows', '3').attr('cols', '22').attr('class', 'note-content').attr('placeholder', 'Content');
-
-
+        
         noteDiv.append(noteTitle);
         noteDiv.append(noteContent);
 
@@ -30,6 +27,7 @@
         ($('<div/>')).addClass('note-content-text').html('Content:' + existingIssueNote.get('content')).appendTo($parent);
         ($('<div/>')).addClass('note-issue-text').html('Issue:' + existingIssueNote.get('issue')).appendTo($parent);
     }
+
     function generateIssueNoteExternal() {
         var $newPiece = $('<li/>');
         var noteBody = generateNoteDiv(index);
@@ -37,28 +35,29 @@
         var $iconRemove = $('<span/>').addClass('glyphicon').addClass('glyphicon-remove').attr('aria-hidden', 'true');
         var $iconSave = $('<span/>').addClass('glyphicon').addClass('glyphicon-ok').attr('aria-hidden', 'true');
         var issueSpecs = $('<div/>').html('<div class="input-group-addon">Issue</div>' +
-        '<input type="text" class="form-control note-issue" placeholder="Description">')
+        '<input type="text" class="form-control note-issue" placeholder="Description">');
 
         noteBody.append(issueSpecs);
 
         $iconRemove.on('click', function () {
-            $(this).parent().fadeOut(300, function () { $(this).remove(); });;
+            $(this).parent().fadeOut(300, function () {
+                $(this).remove();
+            });
         });
 
-
         $iconSave.on('click', function (event) {
-            var $this = $(this);
             var $note = $(noteClassName);
             var $title = $note.find('.note-title');
             var $content = $note.find('.note-content');
             var $issue = $note.find('.note-issue');
+
             ($('<div/>')).addClass('note-title-text').html('Title:' + $title.val()).insertBefore($this);
             ($('<div/>')).addClass('note-content-text').html('Content:' + $content.val()).insertBefore($this);
             ($('<div/>')).addClass('note-issue-text').html('Issue:' + $issue.val()).insertBefore($this);
-            var MyIssueNote = module.getIssueNote($title.val(), $content.val(), $issue.val());
 
+            var MyIssueNote = module.getIssueNote($title.val(), $content.val(), $issue.val());
             var user = Parse.User.current();
-            var IssueNote = Parse.Object.extend("IssueNote")
+            var IssueNote = Parse.Object.extend("IssueNote");
             var storedNote = new IssueNote({
                 idNumber: MyIssueNote.id,
                 title: MyIssueNote.title,
@@ -78,7 +77,6 @@
         })
 
         $newPiece.addClass('gridPiece').addClass('external');
-
         $newPiece.text(index);
         index += 1;
 
@@ -125,11 +123,10 @@
             $(this).parent().fadeOut(300, function(){ $(this).remove();});;
         });
 
-        $newPiece.addClass('gridPiece');
-        
+        $newPiece.addClass('gridPiece');        
         $newPiece.text(index);
         index += 1;
-        
+
         $newPiece.resizable({
             grid: [362, 362], // value to be edited
             autoHide: true,
@@ -158,7 +155,6 @@
         });
 
         $newPiece.addClass('gridPiece');
-
         $newPiece.text(index);
         index += 1;
 
@@ -180,44 +176,38 @@
     });
 
     $inputTypeIssueNote.on('click', function () {
-        var $newPiece = $('<li/>');
-        var noteBody = generateNoteDiv(index);
-        var noteClassName = '.note' + index;
-
         // Emo: Stopped for now because it makes the traversing over the DOM hard.
         //var issueSpecs = $('<div/>').html('<div class="specific"><form class="form-inline"><div class="form-group">' +
         //    '<label class="sr-only" for="exampleInputAmount">Amount (in dollars)</label>' +
         //    '<div class="input-group"><div class="input-group-addon">Issue</div>' +
         //    '<input type="text" class="form-control" placeholder="Description"></div></div></form>' +
         //    '<button type="submit" class="btn btn-primary">Add</button></div>');
+
+        var $newPiece = $('<li/>');
+        var noteBody = generateNoteDiv(index);
+        var noteClassName = '.note' + index;
         var $iconRemove = $('<span/>').addClass('glyphicon').addClass('glyphicon-remove').attr('aria-hidden', 'true');
         var $iconSave = $('<span/>').addClass('glyphicon').addClass('glyphicon-ok').attr('aria-hidden', 'true');
-
-
         var issueSpecs = $('<div/>').html('<div class="input-group-addon">Issue</div>' +
         '<input type="text" class="form-control note-issue" placeholder="Description">')
 
-         noteBody.append(issueSpecs);
+        noteBody.append(issueSpecs);
 
         $iconRemove.on('click', function () {
             $(this).parent().fadeOut(300, function () { $(this).remove(); });;
         });
 
         $iconSave.on('click', function (event) {
-            var $this = $(this);
             var $note = $(noteClassName);
             var $title = $note.find('.note-title');
             var $content = $note.find('.note-content');
             var $issue = $note.find('.note-issue');
-           // $previous.find('.note-title').remove();
-           // $previous.find('.note-content').remove();
-           // $previous.find('.note-issue').remove();
+
             ($('<div/>')).addClass('note-title-text').html('Title:' + $title.val()).insertBefore($this);
             ($('<div/>')).addClass('note-content-text').html('Content:' + $content.val()).insertBefore($this);
             ($('<div/>')).addClass('note-issue-text').html('Issue:' + $issue.val()).insertBefore($this);
+
             var MyIssueNote = module.getIssueNote($title.val(), $content.val(), $issue.val());
-
-
             var user = Parse.User.current();
             var IssueNote = Parse.Object.extend("IssueNote")
             var storedNote = new IssueNote({
@@ -236,15 +226,9 @@
                     alert("Error: " + error.code + " " + error.message);
                 }
             });
-
-
-            //console.log(MyIssueNote.title);
-            //console.log(MyIssueNote.content);
-            //console.log(MyIssueNote.issue);
         })
 
         $newPiece.addClass('gridPiece');
-
         $newPiece.text(index);
         index += 1;
 
