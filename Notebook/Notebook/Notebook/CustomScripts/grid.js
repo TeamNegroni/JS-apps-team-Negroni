e0e3c36c75bc91888fdd7c15820b141c09694a70
Georgegig <george_gig@abv.bg>
2015-08-31 11:54:16 +0300

    grid.js partially refactored

--- Notebook/Notebook/Notebook/CustomScripts/grid.js
+++ Notebook/Notebook/Notebook/CustomScripts/grid.js
@@ -1,4 +1,24 @@
 var index = 1;
+var $grid = $('#grid'),
+    $adderSign = $('#adder-sign');
+var $inputTypes = $('#input-type-wrapper');
+var $inputTypeImage = $('.input-type-image');
+var $inputTypeTextarea = $('.input-type-textarea');
+var $inputTypeIssueNote = $('.input-type-issue-note');
+var $inputTypeMeetingNote = $('.input-type-meeting-note');
+var $inputTypeBankNote = $('.input-type-bank-note');
+var $inputTypeShoppingNote = $('.input-type-shopping-note');
+
+$grid.sortable({
+    containment: 'parent',
+    items: 'li:not(#gridAdder)',
+    cursor: 'move',
+    revert: true,
+    scroll: true,
+    tolerance: 'pointer',
+    placeholder: "sortable-placeholderPiece",
+    forcePlaceholderSize: true
+});
 
 function generateNoteDiv(id) {
     var noteDiv = $('<div/>').attr('class', 'note' + id);
@@ -86,237 +106,222 @@ function generatePreviouslyCreatedIssues(existingIssueNote, count) {
     ($('<div/>')).addClass('note-content-text').html('Content:' + existingIssueNote.get('content')).appendTo($parent);
     ($('<div/>')).addClass('note-issue-text').html('Issue:' + existingIssueNote.get('issue')).appendTo($parent);
 }
+ 
+function generateTextArea() {
+    var $newPiece = $('<li/>');
+    var $textArea = $('<textarea/>').addClass('form-control').attr('row', 3).css('max-width', 300);
+    var $iconRemove = $('<span/>').addClass('glyphicon').addClass('glyphicon-remove').attr('aria-hidden', 'true');
 
-(function () {
-    var $grid = $('#grid'),
-        $adderSign = $('#adder-sign');
-    var $inputTypes = $('#input-type-wrapper');
-    var $inputTypeImage = $('.input-type-image');
-    var $inputTypeTextarea = $('.input-type-textarea');
-    var $inputTypeIssueNote = $('.input-type-issue-note');
-    var $inputTypeMeetingNote = $('.input-type-meeting-note');
-    var $inputTypeBankNote = $('.input-type-bank-note');
-    var $inputTypeShoppingNote = $('.input-type-shopping-note');      
-    
-    $grid.sortable({
-        containment: 'parent',
-        items: 'li:not(#gridAdder)',
-        cursor: 'move',
-        revert: true,
-        scroll: true,
-        tolerance: 'pointer',
-        placeholder: "sortable-placeholderPiece",
-        forcePlaceholderSize: true
+    $iconRemove.on('click', function () {
+        $(this).parent().fadeOut(300, function () { $(this).remove(); });;
     });
 
-    $adderSign.on('click', function () {
-        $adderSign.hide(200);
-        $inputTypes.show(200);
+    $newPiece.addClass('gridPiece');
+    $newPiece.text(index);
+    index += 1;
+
+    $newPiece.resizable({
+        grid: [362, 362], // value to be edited
+        autoHide: true,
+        animate: true,
+        helper: "resizable-helperPiece",
+        animateEasing: "easeInOutQuint"
     });
 
-    $inputTypeTextarea.on('click', function () {
-        var $newPiece = $('<li/>');
-        var $textArea = $('<textarea/>').addClass('form-control').attr('row', 3).css('max-width', 300);
-        var $iconRemove = $('<span/>').addClass('glyphicon').addClass('glyphicon-remove').attr('aria-hidden', 'true');
+    $newPiece.append($iconRemove);
+    $newPiece.append($textArea);
+    $newPiece.hide();
+    $newPiece.insertBefore('#gridAdder');
+    $newPiece.show(500);
+    $inputTypes.hide(200);
+    $adderSign.show(200);
+}
 
-        $iconRemove.on('click', function () {
-            $(this).parent().fadeOut(300, function(){ $(this).remove();});;
-        });
+function generateImageInput() {
+    var $newPiece = $('<li/>');
+    var $input = $('<input/>').attr('type', 'file').attr('accept', 'image/x-png, image/gif, image/jpeg');
+    var $span = $('<span/>').addClass('file-input').addClass('btn').addClass('btn-primary').addClass('btn-file').html('Browse').append($input);
+    var $iconRemove = $('<span/>').addClass('glyphicon').addClass('glyphicon-remove').attr('aria-hidden', 'true');
 
-        $newPiece.addClass('gridPiece');        
-        $newPiece.text(index);
-        index += 1;
-
-        $newPiece.resizable({
-            grid: [362, 362], // value to be edited
-            autoHide: true,
-            animate: true,
-            helper: "resizable-helperPiece",
-            animateEasing: "easeInOutQuint"
-        });
-
-        $newPiece.append($iconRemove);
-        $newPiece.append($textArea);
-        $newPiece.hide();
-        $newPiece.insertBefore('#gridAdder');
-        $newPiece.show(500);
-        $inputTypes.hide(200);
-        $adderSign.show(200);
+    $iconRemove.on('click', function () {
+        $(this).parent().fadeOut(300, function () { $(this).remove(); });;
     });
 
-    $inputTypeImage.on('click', function () {
-        var $newPiece = $('<li/>');
-        var $input = $('<input/>').attr('type', 'file').attr('accept', 'image/x-png, image/gif, image/jpeg');
-        var $span = $('<span/>').addClass('file-input').addClass('btn').addClass('btn-primary').addClass('btn-file').html('Browse').append($input);
-        var $iconRemove = $('<span/>').addClass('glyphicon').addClass('glyphicon-remove').attr('aria-hidden', 'true');
+    $newPiece.addClass('gridPiece');
+    $newPiece.text(index);
+    index += 1;
 
-        $iconRemove.on('click', function () {
-            $(this).parent().fadeOut(300, function () { $(this).remove(); });;
-        });
-
-        $newPiece.addClass('gridPiece');
-        $newPiece.text(index);
-        index += 1;
-
-        $newPiece.resizable({
-            grid: [362, 362], // value to be edited
-            autoHide: true,
-            animate: true,
-            helper: "resizable-helperPiece",
-            animateEasing: "easeInOutQuint"
-        });
-
-        $newPiece.append($iconRemove);
-        $newPiece.append($span);
-        $newPiece.hide();
-        $newPiece.insertBefore('#gridAdder');
-        $newPiece.show(500);
-        $inputTypes.hide(200);
-        $adderSign.show(200);
+    $newPiece.resizable({
+        grid: [362, 362], // value to be edited
+        autoHide: true,
+        animate: true,
+        helper: "resizable-helperPiece",
+        animateEasing: "easeInOutQuint"
     });
 
-    $inputTypeIssueNote.on('click', function () {
+    $newPiece.append($iconRemove);
+    $newPiece.append($span);
+    $newPiece.hide();
+    $newPiece.insertBefore('#gridAdder');
+    $newPiece.show(500);
+    $inputTypes.hide(200);
+    $adderSign.show(200);
+}
+
+$adderSign.on('click', function () {
+    $adderSign.hide(200);
+    $inputTypes.show(200);
+});
+
+$inputTypeTextarea.on('click', function () {
+    generateTextArea();
+});
+
+$inputTypeImage.on('click', function () {
+    generateImageInput();
+ });
+
+$inputTypeIssueNote.on('click', function () {
+     var $this = $(this);
+     var $newPiece = $('<li/>');
+     var noteBody = generateNoteDiv(index);
+     var noteClassName = '.note' + index;
+     var $iconRemove = $('<span/>').addClass('glyphicon').addClass('glyphicon-remove').attr('aria-hidden', 'true');
+     var $iconSave = $('<span/>').addClass('glyphicon').addClass('glyphicon-ok').attr('aria-hidden', 'true');
+     var issueSpecs = $('<div/>').html('<div class="input-group-addon">Issue</div>' +
+     '<input type="text" class="form-control note-issue" placeholder="Description">')
+
+     noteBody.append(issueSpecs);
+
+     $iconRemove.on('click', function () {
+         $(this).parent().fadeOut(300, function () { $(this).remove(); });;
+     });
+
+     $iconSave.on('click', function (event) {
+         var $note = $(noteClassName);
+         var $title = $note.find('.note-title');
+         var $content = $note.find('.note-content');
+         var $issue = $note.find('.note-issue');
+
+         ($('<div/>')).addClass('note-title-text').html('Title:' + $title.val()).insertBefore($this);
+         ($('<div/>')).addClass('note-content-text').html('Content:' + $content.val()).insertBefore($this);
+         ($('<div/>')).addClass('note-issue-text').html('Issue:' + $issue.val()).insertBefore($this);
+
+         var MyIssueNote = module.getIssueNote($title.val(), $content.val(), $issue.val());
+         var user = Parse.User.current();
+         var IssueNote = Parse.Object.extend("IssueNote")
+         var storedNote = new IssueNote({
+             idNumber: MyIssueNote.id,
+             title: MyIssueNote.title,
+             content: MyIssueNote.content,
+             issue: MyIssueNote.issue,
+             user: user
+             });
+
+         storedNote.save(null, {
+             success:function(storedNote){
+                 console.log("successfully saved")
+             },
+             error:function(storedNote,error){
+                 alert("Error: " + error.code + " " + error.message);
+             }
+         });
+     })
+
+     $newPiece.addClass('gridPiece');
+     $newPiece.text(index);
+     index += 1;
+
+     $newPiece.resizable({
+         grid: [362, 362], // value to be edited
+         autoHide: true,
+         animate: true,
+         helper: "resizable-helperPiece",
+         animateEasing: "easeInOutQuint"
+     });
+
+     $newPiece.append($iconRemove);
+     $newPiece.append(noteBody);
+     $newPiece.append($iconSave);
+     $newPiece.hide();
+     $newPiece.insertBefore('#gridAdder');
+     $newPiece.show(500);
+     $inputTypes.hide(200);
+     $adderSign.show(200);
+ });
+
+$inputTypeMeetingNote.on('click', function () {
+    var $newPiece = $('<li/>');
+    var noteBody = generateNoteDiv(index);
+    var noteClassName = '.note' + index;
+    var $iconRemove = $('<span/>').addClass('glyphicon').addClass('glyphicon-remove').attr('aria-hidden', 'true');
+    var $iconSave = $('<span/>').addClass('glyphicon').addClass('glyphicon-ok').attr('aria-hidden', 'true');
+    var meetingSpecs = $('<div/>').html('<div class="input-group-addon">Place</div>' +
+    '<input type="text" class="form-control meeting-place" placeholder="Place">' +
+    '<div class="input-group-addon">Date</div>' +
+    '<input type="text" class="form-control meeting-date" id="datepicker" placeholder="">');
+
+    noteBody.append(meetingSpecs);
+
+    $iconRemove.on('click', function () {
+        $(this).parent().fadeOut(300, function () { $(this).remove(); });;
+    });
+
+    $iconSave.on('click', function (event) {
         var $this = $(this);
-        var $newPiece = $('<li/>');
-        var noteBody = generateNoteDiv(index);
-        var noteClassName = '.note' + index;
-        var $iconRemove = $('<span/>').addClass('glyphicon').addClass('glyphicon-remove').attr('aria-hidden', 'true');
-        var $iconSave = $('<span/>').addClass('glyphicon').addClass('glyphicon-ok').attr('aria-hidden', 'true');
-        var issueSpecs = $('<div/>').html('<div class="input-group-addon">Issue</div>' +
-        '<input type="text" class="form-control note-issue" placeholder="Description">')
+        var $note = $(noteClassName);
+        var $title = $note.find('.note-title');
+        var $content = $note.find('.note-content');
+        var $place = $note.find('.meeting-place');
+        var $date = $note.find('.meeting-date');
 
-        noteBody.append(issueSpecs);
+        ($('<div/>')).addClass('note-title-text').html('Title:' + $title.val()).insertBefore($this);
+        ($('<div/>')).addClass('note-content-text').html('Content:' + $content.val()).insertBefore($this);
+        ($('<div/>')).addClass('meeting-place-text').html('Place:' + $place.val()).insertBefore($this);
+        ($('<div/>')).addClass('meeting-date-text').html('Date:' + $date.val()).insertBefore($this);
 
-        $iconRemove.on('click', function () {
-            $(this).parent().fadeOut(300, function () { $(this).remove(); });;
+        var MyIssueNote = module.getIssueNote($title.val(), $content.val(), $issue.val());
+        var user = Parse.User.current();
+        var IssueNote = Parse.Object.extend("IssueNote")
+        var storedNote = new IssueNote({
+            idNumber: MyIssueNote.id,
+            title: MyIssueNote.title,
+            content: MyIssueNote.content,
+            issue: MyIssueNote.issue,
+            user: user
         });
 
-        $iconSave.on('click', function (event) {
-            var $note = $(noteClassName);
-            var $title = $note.find('.note-title');
-            var $content = $note.find('.note-content');
-            var $issue = $note.find('.note-issue');
-
-            ($('<div/>')).addClass('note-title-text').html('Title:' + $title.val()).insertBefore($this);
-            ($('<div/>')).addClass('note-content-text').html('Content:' + $content.val()).insertBefore($this);
-            ($('<div/>')).addClass('note-issue-text').html('Issue:' + $issue.val()).insertBefore($this);
-
-            var MyIssueNote = module.getIssueNote($title.val(), $content.val(), $issue.val());
-            var user = Parse.User.current();
-            var IssueNote = Parse.Object.extend("IssueNote")
-            var storedNote = new IssueNote({
-                idNumber: MyIssueNote.id,
-                title: MyIssueNote.title,
-                content: MyIssueNote.content,
-                issue: MyIssueNote.issue,
-                user: user
-                });
-
-            storedNote.save(null, {
-                success:function(storedNote){
-                    console.log("successfully saved")
-                },
-                error:function(storedNote,error){
-                    alert("Error: " + error.code + " " + error.message);
-                }
-            });
-        })
-
-        $newPiece.addClass('gridPiece');
-        $newPiece.text(index);
-        index += 1;
-
-        $newPiece.resizable({
-            grid: [362, 362], // value to be edited
-            autoHide: true,
-            animate: true,
-            helper: "resizable-helperPiece",
-            animateEasing: "easeInOutQuint"
+        storedNote.save(null, {
+            success: function (storedNote) {
+                console.log("successfully saved")
+            },
+            error: function (storedNote, error) {
+                alert("Error: " + error.code + " " + error.message);
+            }
         });
+    })
 
-        $newPiece.append($iconRemove);
-        $newPiece.append(noteBody);
-        $newPiece.append($iconSave);
-        $newPiece.hide();
-        $newPiece.insertBefore('#gridAdder');
-        $newPiece.show(500);
-        $inputTypes.hide(200);
-        $adderSign.show(200);
+    $newPiece.addClass('gridPiece');
+    $newPiece.text(index);
+    index += 1;
+
+    $newPiece.resizable({
+        grid: [362, 362], // value to be edited
+        autoHide: true,
+        animate: true,
+        helper: "resizable-helperPiece",
+        animateEasing: "easeInOutQuint"
     });
 
-    $inputTypeMeetingNote.on('click', function () {
-        var $newPiece = $('<li/>');
-        var noteBody = generateNoteDiv(index);
-        var noteClassName = '.note' + index;
-        var $iconRemove = $('<span/>').addClass('glyphicon').addClass('glyphicon-remove').attr('aria-hidden', 'true');
-        var $iconSave = $('<span/>').addClass('glyphicon').addClass('glyphicon-ok').attr('aria-hidden', 'true');
-        var meetingSpecs = $('<div/>').html('<div class="input-group-addon">Place</div>' +
-        '<input type="text" class="form-control meeting-place" placeholder="Place">' +
-        '<div class="input-group-addon">Date</div>' +
-        '<input type="text" class="form-control meeting-date" id="datepicker" placeholder="">');
-
-        noteBody.append(meetingSpecs);
-
-        $iconRemove.on('click', function () {
-            $(this).parent().fadeOut(300, function () { $(this).remove(); });;
-        });
-
-        $iconSave.on('click', function (event) {
-            var $this = $(this);
-            var $note = $(noteClassName);
-            var $title = $note.find('.note-title');
-            var $content = $note.find('.note-content');
-            var $place = $note.find('.meeting-place');
-            var $date = $note.find('.meeting-date');
-
-            ($('<div/>')).addClass('note-title-text').html('Title:' + $title.val()).insertBefore($this);
-            ($('<div/>')).addClass('note-content-text').html('Content:' + $content.val()).insertBefore($this);
-            ($('<div/>')).addClass('meeting-place-text').html('Place:' + $place.val()).insertBefore($this);
-            ($('<div/>')).addClass('meeting-date-text').html('Date:' + $date.val()).insertBefore($this);
-
-            var MyIssueNote = module.getIssueNote($title.val(), $content.val(), $issue.val());
-            var user = Parse.User.current();
-            var IssueNote = Parse.Object.extend("IssueNote")
-            var storedNote = new IssueNote({
-                idNumber: MyIssueNote.id,
-                title: MyIssueNote.title,
-                content: MyIssueNote.content,
-                issue: MyIssueNote.issue,
-                user: user
-            });
-
-            storedNote.save(null, {
-                success: function (storedNote) {
-                    console.log("successfully saved")
-                },
-                error: function (storedNote, error) {
-                    alert("Error: " + error.code + " " + error.message);
-                }
-            });
-        })
-
-        $newPiece.addClass('gridPiece');
-        $newPiece.text(index);
-        index += 1;
-
-        $newPiece.resizable({
-            grid: [362, 362], // value to be edited
-            autoHide: true,
-            animate: true,
-            helper: "resizable-helperPiece",
-            animateEasing: "easeInOutQuint"
-        });
-
-        $newPiece.append($iconRemove);
-        $newPiece.append(noteBody);
-        $newPiece.append($iconSave);
-        $newPiece.hide();
-        $newPiece.insertBefore('#gridAdder');
-        $newPiece.show(500);
-        $inputTypes.hide(200);
-        $adderSign.show(200);
-    });
-}());
+    $newPiece.append($iconRemove);
+    $newPiece.append(noteBody);
+    $newPiece.append($iconSave);
+    $newPiece.hide();
+    $newPiece.insertBefore('#gridAdder');
+    $newPiece.show(500);
+    $inputTypes.hide(200);
+    $adderSign.show(200);
+});
 
 $(document).on('change', '.btn-file :file', function () {
     var input = $(this),
